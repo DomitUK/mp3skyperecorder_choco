fe383cbd58d0ffec313e34217a89046dcd0c1359
CAIIIA <alexander@nikiforov.eu>
2019-03-20 00:59:32 +0300

    Initial

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'; 
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$fileLocation = Join-Path $toolsDir 'MP3SkypeRecorderSetup.msi'
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  file          = $fileLocation
+  fileType      = 'MSI' 
+  softwareName  = 'MP3 Skype recorder*' 
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" 
+  validExitCodes= @(0,1641,3010)
+}
+
+Install-ChocolateyPackage @packageArgs 
+Remove-Item -Force $packageArgs.file
